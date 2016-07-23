60283678885d3738be934e32f0ff82e265c59790
zxyq <511292331@qq.com>
2016-07-23 22:01:00 +0800

    完善1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,14 @@
-$(function () {
-    $("#fullpage").fullpage({
-        // 与顶部的距离
-        paddingTop: '60px',
-        // 固定的元素 使在使用fullpage时候销毁该元素的fullpage效果
-        fixedElements: '#header',
-        // 显示项目导航
-        navigation: true,
-        // 定义锚链接
-        anchors: ['百度舆情', '领先', '全面', '专业'],
-        // 项目导航的tip
-        navigationTooltips: ['百度舆情', '领先', '全面', '专业'],
-        // showActiveTooltip:true,
-    });
-    $('.btn-arrow').on('click', function () {
-        // 向下滚动
-        $.fn.fullpage.moveSectionDown();
-    })
+
+$(function(){
+	$("#fullpage").fullpage({
+		paddingTop:'60px',
+		fixedElements:'#header',
+		navigation:true,
+		anchors:['百度舆情','领先','全面','专业'],
+		navigationTooltips:['百度舆情','领先','全面','专业'],
+		// showActiveTooltip:true,
+	});
+	$('.btn-arrow').on('click',function(){
+		$.fn.fullpage.moveSectionDown();
+	})
 })
--- js/lunbo.js
+++ js/lunbo.js
@@ -1,40 +1,38 @@
-(function ($) {
-    var _config = {
-        interval: 2000, stepin: $.noop
-    }
-    $.fn.extend({
-        lunbo: function (config) {
+(function($){
+	var _config={
+			interval:2000,stepin:$.noop
+		}
+	$.fn.extend({lunbo:function(config){
 
-            $.extend(_config, config)
-            var ul = $(this).find('ul');
-            li = ul.find('li');
-            off = li.outerWidth(true),
-                index = 1;
+		$.extend(_config,config)
+		var ul = $(this).find('ul');
+	    li = ul.find('li');
+	    off = li.outerWidth(true),
+	    index = 1;
 
-            var move = function () {
-                // console.log(off)
-                ul
-                    .animate({marginLeft: -off}, 2000, function () {
-                        $(this).css('marginLeft', 0)
-                            .append(ul.find(':first'))
-                    })
-                index += 1;
-                _config.stepin(index);
-                if (index == li.length) {
-                    index = 0;
-                }
-            }
-            var timerid = setInterval(move, _config.interval)
-            $(this).on('mouseenter.lunbo', function () {
-                clearInterval(timerid);
-            })
+	    var move = function(){
+	    	// console.log(off)
+	    	ul	    	
+	    	.animate({marginLeft:-off},2000,function(){
+	    		$(this).css('marginLeft',0)
+	    	.append(ul.find(':first'))
+	    	})
+	    	index += 1;
+	    	_config.stepin(index);
+	    	if(index == li.length){
+	    		index = 0;
+	    	}
+	    }
+	   var timerid = setInterval(move,_config.interval)
+	    $(this).on('mouseenter.lunbo',function(){
+	    	clearInterval(timerid);
+	    })
 
-            $(this).on('mouseleave.lunbo', function () {
-                timerid = setInterval(move, _config.interval)
-            })
-        }
-    })
-
+	    $(this).on('mouseleave.lunbo',function(){
+	    	timerid = setInterval(move,_config.interval)
+	    })
+	}})
+	
 
 })(jQuery);
 
